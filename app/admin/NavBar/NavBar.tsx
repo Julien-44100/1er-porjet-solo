0f4b6d62a3550b64d047973a474e01bc84d9763d
Julien-44100 <julienbotar44100@yahoo.com>
2025-03-05 20:15:41 +0100

    add navbar

---
+++ app/admin/NavBar/NavBar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import styles from "./NavBar.module.css";
+
+interface NavBarProps {
+	user?: unknown;
+}
+
+export default function NavBar({ user }: NavBarProps) {
+	const [alternator, setAlternator] = useState<[boolean, boolean]>([
+		true,
+		false,
+	]);
+
+	useEffect(() => {
+		setAlternator(!user ? [true, false] : [false, true]);
+	}, [user]);
+
+	return (
+		<div className={styles.navbar}>
+			{alternator[1] && <button type="button">Connexion</button>}
+			{alternator[0] && <button type="button">Inscription</button>}
+		</div>
+	);
+}
