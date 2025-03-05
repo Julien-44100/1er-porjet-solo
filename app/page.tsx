7af0083d551124ee2b27c8c8e81a681bf8a214d3
Julien-44100 <julienbotar44100@yahoo.com>
2025-03-05 20:33:58 +0100

    add text commit for page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
 	return (
 		<div className={styles.hi}>
-			<h1>sjijisahid</h1>
+			<h1>jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</h1>
 		</div>
 	);
 }
