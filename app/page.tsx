7f872e086f4fbc66b65b0d2b7f63ae0469207228
Julien-44100 <julienbotar44100@yahoo.com>
2025-03-05 20:32:41 +0100

    remive commit for page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
+import Image from "next/image";
+import styles from "./page.module.css";
 
-// export default function Home() {
-// 	return (
-// 		<div className={styles.hi}>
-// 			<h1>sjijisahid</h1>
-// 		</div>
-// 	);
-// }
+export default function Home() {
+	return (
+		<div className={styles.hi}>
+			<h1>sjijisahid</h1>
+		</div>
+	);
+}
