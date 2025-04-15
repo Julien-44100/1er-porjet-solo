e43ee632c5dc6a7558e72bbf130d4129ebc5b492
Julien-44100 <julienbotar44100@yahoo.com>
2025-04-15 13:30:50 +0200

    resolve problem erreur interne du serveur

--- app/connexion/Connexion.tsx
+++ app/connexion/Connexion.tsx
@@ -1,47 +1,47 @@
-// "use client";
+"use client";
 
-// import { useRouter } from "next/navigation";
-// import styles from "./Connexion.module.css";
+import { useRouter } from "next/navigation";
+import styles from "./Connexion.module.css";
 
-// export default function Connexion() {
-// 	const router = useRouter();
+export default function Connexion() {
+	const router = useRouter();
 
-// 	const handleClick = (e: React.FormEvent) => {
-// 		e.preventDefault();
-// 		// router.push("/Actuality");
-// 	};
+	const handleClick = (e: React.FormEvent) => {
+		e.preventDefault();
+		// router.push("/Actuality");
+	};
 
-// 	return (
-// 		<div className={styles.connexion}>
-// 			<form className={styles.cardConnexion}>
-// 				<h1 className={styles.connexionH1}>Connexion</h1>
+	return (
+		<div className={styles.connexion}>
+			<form className={styles.cardConnexion}>
+				<h1 className={styles.connexionH1}>Connexion</h1>
 
-// 				<label htmlFor="email">Adresse mail</label>
-// 				<input
-// 					type="email"
-// 					id="email"
-// 					name="email"
-// 					placeholder="Enter your email"
-// 					required
-// 					className={styles.input}
-// 				/>
+				<label htmlFor="email">Adresse mail</label>
+				<input
+					type="email"
+					id="email"
+					name="email"
+					placeholder="Enter your email"
+					required
+					className={styles.input}
+				/>
 
-// 				<label htmlFor="password" className={styles.password}>
-// 					Mot de passe
-// 				</label>
-// 				<input
-// 					type="password"
-// 					id="password"
-// 					name="password"
-// 					placeholder="Enter your password"
-// 					required
-// 					className={styles.input}
-// 				/>
+				<label htmlFor="password" className={styles.password}>
+					Mot de passe
+				</label>
+				<input
+					type="password"
+					id="password"
+					name="password"
+					placeholder="Enter your password"
+					required
+					className={styles.input}
+				/>
 
-// 				<button type="button" onClick={handleClick} className={styles.submit}>
-// 					Se connecter
-// 				</button>
-// 			</form>
-// 		</div>
-// 	);
-// }
+				<button type="button" onClick={handleClick} className={styles.submit}>
+					Se connecter
+				</button>
+			</form>
+		</div>
+	);
+}
