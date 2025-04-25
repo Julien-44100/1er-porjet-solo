cd0a51e23c6f3fc89f084e25c5ea276f82300a03
Julien-44100 <julienbotar44100@yahoo.com>
2025-04-25 16:10:57 +0200

    add actuality page

---
+++ app/actuality/Actuality.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./Actuality.module.css";
+
+export default function Actualitypage() {
+	return (
+		<div className={styles.Actuality}>
+			<h1>Bonjour</h1>
+		</div>
+	);
+}
---
+++ app/actuality/page.tsx
@@ -0,0 +1,13 @@
+// app/actuality/page.tsx
+"use client";
+
+import styles from "./Actuality.module.css";
+
+export default function ActualityPage() {
+	return (
+		<main className={styles.container}>
+			<h1>Actualités</h1>
+			<p>Bienvenue sur la page Actualité !</p>
+		</main>
+	);
+}
